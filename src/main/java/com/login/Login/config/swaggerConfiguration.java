665da526e7e87ed30d7de3a005553ab6c76f8e5d
Armel <armelbedjra12@gmail.com>
2025-06-02 12:35:30 +0000

    Description

---
+++ src/main/java/com/login/Login/config/swaggerConfiguration.java
@@ -0,0 +1,17 @@
+package com.login.Login.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class swaggerConfiguration {
+
+//    @Bean
+//    public OpenAPI customOpenAPI() {
+//        return new OpenAPI()
+//                .info(new Info()
+//                        .title("Login API")
+//                        .version("1")
+//                        .description("Documentation de l'API Login"));
+//    }
+}

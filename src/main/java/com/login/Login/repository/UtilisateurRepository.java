4b934e252a93028c5c1987da2b926488877c901b
Armel <armelbedjra12@gmail.com>
2025-06-02 12:40:53 +0000

    Description

--- src/main/java/com/login/Login/repository/UtilisateurRepository.java
+++ src/main/java/com/login/Login/repository/UtilisateurRepository.java
@@ -3,7 +3,11 @@ package com.login.Login.repository;
 import com.login.Login.Entity.Utilisateur;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UtilisateurRepository extends JpaRepository<Utilisateur, Long> {
 
-  }
+    Optional<Utilisateur> findbyEmail(String email);
+    
+}
 
--- src/main/java/com/login/Login/service/UtilisateurService.java
+++ src/main/java/com/login/Login/service/UtilisateurService.java
@@ -1,9 +1,6 @@
 package com.login.Login.service;
 
-import com.login.Login.Entity.Utilisateur;
 import com.login.Login.repository.UtilisateurRepository;
-import jakarta.persistence.EntityNotFoundException;
-import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
